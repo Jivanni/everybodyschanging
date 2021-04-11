74aa0aa1f7d789e112e085e5517b0631ffd95022
Jivanni <60815162+Jivanni@users.noreply.github.com>
2021-04-11 08:56:37 +0200

    some fixes

--- billboard_scraper/billboard_scraper.py
+++ billboard_scraper/billboard_scraper.py
@@ -1,6 +1,7 @@
 import billboard
 import csv
 from datetime import date
+import os
 
 #requires https://github.com/guoguo12/billboard-charts
 
@@ -10,28 +11,31 @@ END_DATE = '2020-06-28' #starts downloading weekly charts from END_DATE backward
 DOWNLOAD_DIR = "downloads"
 FIELD_NAMES = ["rank", "title", "artist"]
 
+start = date.fromisoformat(START_DATE)
+curr = END_DATE
 
-if __name__ == "__main__":
-	start = date.fromisoformat(START_DATE)
-	curr = END_DATE
+try:
+	os.mkdir(DOWNLOAD_DIR + "/" + CHART_NAME)
+except FileExistsError:
+	pass
 
-	while date.fromisoformat(curr) >= start:
-		curr_chart = billboard.ChartData(CHART_NAME, curr)
-		csv_filename = DOWNLOAD_DIR + "/" + curr + "_" + CHART_NAME + ".csv"
+while date.fromisoformat(curr) >= start:
+	curr_chart = billboard.ChartData(CHART_NAME, curr)
+	csv_filename = DOWNLOAD_DIR + "/" + CHART_NAME + "/" + curr + "_" + CHART_NAME + ".csv"
 
-		with open(csv_filename, "w", newline='') as file_handler:
-			writer = csv.writer(file_handler, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-			writer.writerow(FIELD_NAMES)
-			for song in curr_chart:
-				writer.writerow(
-					[song.rank, song.title, song.artist]
-				)
-		print(curr)
-		with open("log.txt", "a+") as logfile:
-			logfile.write("\n" + curr)
-		curr = curr_chart.previousDate
+	with open(csv_filename, "w", newline='') as file_handler:
+		writer = csv.writer(file_handler, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+		writer.writerow(FIELD_NAMES)
+		for song in curr_chart:
+			writer.writerow(
+				[song.rank, song.title, song.artist]
+			)
+	print(curr)
+	with open("log.txt", "a+") as logfile:
+		logfile.write("\n" + curr)
+	curr = curr_chart.previousDate
 
-	print("finished")
+print("finished")
 
 
 
